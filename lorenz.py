5b1825d4b5cb6c960c576b322e7aa5ec6058ffaf
Mike Dusseault <mike.dusseault@gmail.com>
2012-04-21 01:50:37 -0400

    Added the author's name, me. Seemed like an oversight.

--- lorenz.py
+++ lorenz.py
@@ -14,6 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Lorenz SZ40/42 Cipher Machine simulator
+#
+# Author(s): Mike Dusseault
+#
+
 import sys
 import random
 
